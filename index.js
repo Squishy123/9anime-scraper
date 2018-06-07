58454b6c8a31b8187e0e6f8076b8afb193e74218
Squishy123 <christian.gnaw@gmail.com>
2018-06-06 22:47:27 -0400

    Added error checking for request default

--- index.js
+++ index.js
@@ -49,61 +49,117 @@ module.exports = {
     * @param {String} url 
     */
     getSource: function (url, options, cb) {
-        if(options)
-            request.defaults(options)
-        request(url, (err, res, body) => {
-            if(!err) {
-                const $ = cheerio.load(body);
-                let sources = [];
-                for (let p = 1; p <= $('#main > div > div.widget.servers > div.widget-body').children().length; p++) {
-                    let id = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p})`).attr('data-id');
-                    sources.push({ id: id, sourceList: [] });
-                    //range
-                    let range = ($(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).length) ? $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).children().length : 1;
-                    for (let r = 0; r < range; r++) {
-                        if (range == 1) {
-                            let list = [];
-                            let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul`).children().length;
-                            for (let i = 1; i <= listLength; i++) {
-                                list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul > li:nth-child(${i}) > a`).attr('href'), index: i });
+        if (options) {
+            request.defaults(options)(url, (err, res, body) => {
+                if (!err) {
+                    const $ = cheerio.load(body);
+                    let sources = [];
+                    for (let p = 1; p <= $('#main > div > div.widget.servers > div.widget-body').children().length; p++) {
+                        let id = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p})`).attr('data-id');
+                        sources.push({ id: id, sourceList: [] });
+                        //range
+                        let range = ($(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).length) ? $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).children().length : 1;
+                        for (let r = 0; r < range; r++) {
+                            if (range == 1) {
+                                let list = [];
+                                let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul`).children().length;
+                                for (let i = 1; i <= listLength; i++) {
+                                    list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul > li:nth-child(${i}) > a`).attr('href'), index: i });
+                                }
+                                sources[p - 1].sourceList = list;
+                            } else {
+                                let list = [];
+                                let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2})`).children().length;
+                                for (let i = 1; i <= listLength; i++) {
+                                    list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2}) > li:nth-child(${i}) > a`).attr('href'), index: i });
+                                }
+                                sources[p - 1].sourceList.push(...list);
                             }
-                            sources[p - 1].sourceList = list;
-                        } else {
-                            let list = [];
-                            let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2})`).children().length;
-                            for (let i = 1; i <= listLength; i++) {
-                                list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2}) > li:nth-child(${i}) > a`).attr('href'), index: i });
-                            }
-                            sources[p - 1].sourceList.push(...list);
                         }
                     }
+                    cb(sources);
+                } else {
+                    cb(err)
                 }
-                cb(sources);
-             }
-        });
+            });
+        } else {
+            request(url, (err, res, body) => {
+                if (!err) {
+                    const $ = cheerio.load(body);
+                    let sources = [];
+                    for (let p = 1; p <= $('#main > div > div.widget.servers > div.widget-body').children().length; p++) {
+                        let id = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p})`).attr('data-id');
+                        sources.push({ id: id, sourceList: [] });
+                        //range
+                        let range = ($(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).length) ? $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > div`).children().length : 1;
+                        for (let r = 0; r < range; r++) {
+                            if (range == 1) {
+                                let list = [];
+                                let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul`).children().length;
+                                for (let i = 1; i <= listLength; i++) {
+                                    list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul > li:nth-child(${i}) > a`).attr('href'), index: i });
+                                }
+                                sources[p - 1].sourceList = list;
+                            } else {
+                                let list = [];
+                                let listLength = $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2})`).children().length;
+                                for (let i = 1; i <= listLength; i++) {
+                                    list.push({ href: $(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p}) > ul:nth-child(${r + 2}) > li:nth-child(${i}) > a`).attr('href'), index: i });
+                                }
+                                sources[p - 1].sourceList.push(...list);
+                            }
+                        }
+                    }
+                    cb(sources);
+                } else {
+                    cb(err)
+                }
+            });
+        }
     },
     /**
      * Returns the links of animes under a keyword search 
      */
     getSearch: async function (keyword, options, cb) {
-        if(options)
-            request.defaults(options)
-        request(`https://www4.9anime.is/search?keyword=${keyword}`, (err, res, body) => {
-        if (!err) {
-            const $ = cheerio.load(body);
-            let results = [];
-            let length = $('#main > div > div:nth-child(1) > div.widget-body > div.film-list').children().length;
-            for (let c = 1; c <= length; c++) {
-            if ($(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href') && $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle'))
-                results.push({
-                poster: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div`).find(`a.poster > img`).attr('src'),
-                href: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href'),
-                title: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle')
-                });
-            }
-            cb(results);
+        if (options) {
+            request.defaults(options)(`https://www4.9anime.is/search?keyword=${keyword}`, (err, res, body) => {
+                if (!err) {
+                    const $ = cheerio.load(body);
+                    let results = [];
+                    let length = $('#main > div > div:nth-child(1) > div.widget-body > div.film-list').children().length;
+                    for (let c = 1; c <= length; c++) {
+                        if ($(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href') && $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle'))
+                            results.push({
+                                poster: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div`).find(`a.poster > img`).attr('src'),
+                                href: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href'),
+                                title: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle')
+                            });
+                    }
+                    cb(results);
+                } else {
+                    cb(err)
+                }
+            });
+        } else {
+            request(`https://www4.9anime.is/search?keyword=${keyword}`, (err, res, body) => {
+                if (!err) {
+                    const $ = cheerio.load(body);
+                    let results = [];
+                    let length = $('#main > div > div:nth-child(1) > div.widget-body > div.film-list').children().length;
+                    for (let c = 1; c <= length; c++) {
+                        if ($(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href') && $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle'))
+                            results.push({
+                                poster: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div`).find(`a.poster > img`).attr('src'),
+                                href: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('href'),
+                                title: $(`#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${c}) > div > a.name`).attr('data-jtitle')
+                            });
+                    }
+                    cb(results);
+                } else {
+                    cb(err)
+                }
+            });
         }
-    });
     }
 }
 
--- tests/getSourceTest.js
+++ tests/getSourceTest.js
@@ -5,8 +5,11 @@ const util = require('util');
 (async () => {
     console.log(`Test 1: getSource()`)
     let start = new Date();
-    let sources = await scrape.getSource('https://www5.9anime.is/watch/neon-genesis-evangelion-dub.yk0z/r92rwn', null, (sources) => {
+    let sources = await scrape.getSource('https://www5.9anime.is/watch/neon-genesis-evangelion-dub.yk0z/x6kxv3', null, (sources) => {    
+    if(sources instanceof Error)
+        console.log("Error")
+    else 
         console.log(util.inspect(sources, false, null));
-        console.log(`Test 1: getSource() - Execution Time: ${new Date() - start}`);
+    console.log(`Test 1: getSource() - Execution Time: ${new Date() - start}`);
     });
 })();
